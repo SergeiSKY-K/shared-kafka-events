9037a4b244e3bf20430d6ea1ba5ec37f4e3563f0
Sergei K <kazandzhyanserzh@gmail.com>
2025-12-19 14:06:29 +0200

    Release 1.0.6

--- src/main/java/telran/java57/sharedkafkaevents/kafka/events/OrderItemEvent.java
+++ src/main/java/telran/java57/sharedkafkaevents/kafka/events/OrderItemEvent.java
@@ -5,20 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.Instant;
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class OrderPaidEvent {
+public class OrderItemEvent {
 
-    private String orderId;
-
-    private List<OrderItemEvent> items;
-
-    private Instant paidAt;
-
-    private int version;
+    private String productId;
+    private int quantity;
 }
