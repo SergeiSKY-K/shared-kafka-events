f23df95d450f61f9c9f81cf0d73cd7aabf5e0801
Sergei K <kazandzhyanserzh@gmail.com>
2025-12-19 11:57:15 +0200

    Add OrderPaidEvent with Lombok

--- src/main/java/telran/java57/sharedkafkaevents/kafka/events/OrderPaidEvent.java
+++ src/main/java/telran/java57/sharedkafkaevents/kafka/events/OrderPaidEvent.java
@@ -1,8 +1,16 @@
 package telran.java57.sharedkafkaevents.kafka.events;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.Instant;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderPaidEvent {
+
     private String orderId;
     private String userLogin;
     private double totalPrice;
